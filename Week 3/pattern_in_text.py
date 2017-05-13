5723bb86ba0f724a5475a85c55c39864e8ee6911
Vladislav Pakhomov <vladislav.pakhomov@bostongene.com>
2017-05-14 00:43:19 +0300

    simplified hash func

--- Week 3/pattern_in_text.py
+++ Week 3/pattern_in_text.py
@@ -1,28 +1,20 @@
 import sys
 
 
-def hash(string, x):
-    p = 1000000007
-    res = 0
-    last_pow_x = 1
-    for i, char in enumerate(string):
-        res += (ord(char) * last_pow_x) % p
-        last_pow_x = (last_pow_x * x) % p
-    res %= p
-    return res
+def hash(string):
+    return sum(map(ord, string))
 
 
-def get_text_hashes(text, window_size, x):
-    p = 1000000007
+def get_text_hashes(text, window_size):
 
-    first_window = hash(text[-window_size:], x)
+    first_window = hash(text[-window_size:])
     last_char = text[-1]
 
     ret = [first_window, ]
 
     for i in range(len(text)-window_size)[::-1]:
         last_hash = ret[-1]
-        new_hash = ((last_hash - ord(last_char) * ((x ** (window_size-1))) % p) * x + ord(text[i])) % p
+        new_hash = (last_hash - ord(last_char)) + ord(text[i])
         ret.append(new_hash)
         last_char = text[i+window_size-1]
     return ret[::-1]
@@ -32,13 +24,11 @@ def main():
     pattern = sys.stdin.readline().strip()
     text = sys.stdin.readline().strip()
 
-    x = len(text)
-
-    pattern_hash = hash(pattern, x)
-    text_hashes = get_text_hashes(text, len(pattern), x)
+    pattern_hash = hash(pattern)
+    text_hashes = get_text_hashes(text, len(pattern))
 
     for i in range(len(text_hashes)):
-        if text_hashes[i] == pattern_hash and text[i:i+len(pattern)] == pattern:
+        if text_hashes[i] == pattern_hash and text[i] == pattern[0] and text[i:i+len(pattern)] == pattern:
             print(i, end=' ')
 
 
